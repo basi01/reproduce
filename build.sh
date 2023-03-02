589e069b3fbfd2bed6c8c87aee5743e90f573b9a
Ilya Basin <basinilya@gmail.com>
2023-03-02 20:32:01 +0300

    change svn branch

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "get latest Revision from branch Development"
-SVN_REV=`svn info  --show-item=last-changed-revision https://teamserver.sprintr.com/$SVN_PROJECT_ID/branches/Development --username $SVN_USERNAME --password $SVN_PASSWORD`
+#echo "get latest Revision from branch Development"
+#SVN_REV=`svn info  --show-item=last-changed-revision https://teamserver.sprintr.com/$SVN_PROJECT_ID/trunk --username $SVN_USERNAME --password $SVN_PASSWORD`
+SVN_REV=
+
+SVN_RARG=${SVN_REV:+-r ${SVN_REV}}
+
 echo "Checking out code (revision $SVN_REV) from: https://teamserver.sprintr.com/$SVN_PROJECT_ID/branches/Development under $SVN_USERNAME" 
-svn checkout -q -r $SVN_REV https://teamserver.sprintr.com/$SVN_PROJECT_ID/branches/Development checkout/src --username $SVN_USERNAME --password $SVN_PASSWORD 
+svn checkout -q ${SVN_RARG} https://teamserver.sprintr.com/$SVN_PROJECT_ID/trunk checkout/src --username $SVN_USERNAME --password $SVN_PASSWORD 
 rm -rf checkout/src/.svn 
 
 CURR_DIR=`pwd`
