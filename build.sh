ccb53afc45ca2bb3bfcbfe0d639bd42d9cae7fa1
Ilya Basin <basinilya@gmail.com>
2023-03-03 16:05:46 +0300

    add Mendix constants

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+SVN_URL="https://teamserver.sprintr.com/${SVN_PROJECT_ID:?}/trunk"
+
 #echo "get latest Revision from branch Development"
-#SVN_REV=`svn info  --show-item=last-changed-revision https://teamserver.sprintr.com/$SVN_PROJECT_ID/trunk --username $SVN_USERNAME --password $SVN_PASSWORD`
+#SVN_REV=`svn info --show-item=last-changed-revision "$SVN_URL" --username "$SVN_USERNAME" --password "$SVN_PASSWORD"`
 SVN_REV=
 
 SVN_RARG=${SVN_REV:+-r ${SVN_REV}}
 
-echo "Checking out code (revision $SVN_REV) from: https://teamserver.sprintr.com/$SVN_PROJECT_ID/branches/Development under $SVN_USERNAME" 
-svn checkout -q ${SVN_RARG} https://teamserver.sprintr.com/$SVN_PROJECT_ID/trunk checkout/src --username $SVN_USERNAME --password $SVN_PASSWORD 
-rm -rf checkout/src/.svn 
+echo "Checking out code ($SVN_RARG) from: "$SVN_URL" under $SVN_USERNAME"
+svn checkout -q ${SVN_RARG} "$SVN_URL" checkout/src --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+rm -rf checkout/src/.svn
 
 CURR_DIR=`pwd`
 
